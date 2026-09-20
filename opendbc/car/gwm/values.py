45e60fb1dfd737ca716d25e19a2479d14e6905ec
Marco Ribeiro <marco@MacBook-Air-de-Marco.local>
2026-09-19 21:12:00 -0300

    gwm: add fw_version_regex required by FwQueryConfig

--- opendbc/car/gwm/values.py
+++ opendbc/car/gwm/values.py
@@ -97,6 +97,7 @@ GREATWALLMOTORS_VERSION_RESPONSE = bytes([uds.SERVICE_TYPE.READ_DATA_BY_IDENTIFI
 GREATWALLMOTORS_RX_OFFSET = 0x6a
 
 FW_QUERY_CONFIG = FwQueryConfig(
+  fw_version_regex=br"[\x00-\xff]*",
   requests=[request for bus, obd_multiplexing in [(1, True), (1, False), (0, False)] for request in [
     Request(
       [GREATWALLMOTORS_VERSION_REQUEST_MULTI],
